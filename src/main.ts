1811dd4cc3ec485c9fd3cd0b885b12b6d7ed4502
m-a-mahammad <m.a.mahammad2020@gmail.com>
2025-09-15 12:41:01 +0300

    Implement security-related libraries [cors, helmet, xss]

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cors from 'cors';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.use(
+    cors({
+      origin: process.env.CLIENT_URL,
+      credentials: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
