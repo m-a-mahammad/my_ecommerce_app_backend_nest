7fe6b1c9ba0c72d21c8e2caefce3960ad1308073
m-a-mahammad <m.a.mahammad2020@gmail.com>
2025-09-15 15:52:21 +0300

    feat(auth): implement logout endpoint with cookie clearing and session validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,20 +2,24 @@ import {
   BadRequestException,
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   Post,
   Req,
+  Res,
   UnauthorizedException,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { SetAuthCookieInterceptor } from 'src/interceptors/set-auth-cookie.interceptor';
+import { RequestWithCookies } from 'src/interfaces/request-with-cookies.interface';
+import { isProduction } from 'src/utils/constants';
 
 @Controller('users')
 export class UsersController {
@@ -63,5 +67,30 @@ export class UsersController {
     }
 
     return user;
+  }
+
+  @Delete('logout')
+  @UseGuards(AuthGuard)
+  logoutUser(
+    @Req() req: RequestWithCookies,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const userId = req.userId;
+    if (!userId) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    const token = req.cookies.token;
+    if (!token) {
+      throw new UnauthorizedException('User not logged in');
+    }
+
+    res.clearCookie('token', {
+      httpOnly: true,
+      sameSite: isProduction ? 'none' : 'lax',
+      secure: isProduction ? true : false,
+    });
+
+    return { message: 'User logged out successfully' };
   }
 }
