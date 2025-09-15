efad99c5cf2fe8efcd7829c22313264967c927bc
m-a-mahammad <m.a.mahammad2020@gmail.com>
2025-09-15 13:36:04 +0300

    Create user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,42 @@
-export class User {}
+import { UserRole } from 'src/enums/user-role.enum';
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+class Image {
+  @Column({ name: 'image_url', nullable: true })
+  url: string;
+}
+
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    length: 100,
+  })
+  name: string;
+
+  @Column({ type: 'varchar', unique: true, nullable: false })
+  email: string;
+
+  @Column({ type: 'varchar', nullable: false, length: 100 })
+  password: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.USER })
+  role: string;
+
+  @Column(() => Image, { prefix: false })
+  image: Image;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
