d59e0bd293a3b0ac82d3d67c53248887699fa814
m-a-mahammad <m.a.mahammad2020@gmail.com>
2025-09-16 08:34:47 +0300

    feat(wishlist): add product to user wishlist with conflict check and structured response

--- src/wishlists/wishlists.controller.ts
+++ src/wishlists/wishlists.controller.ts
@@ -1,13 +1,16 @@
 import {
+  Body,
   Controller,
   Get,
   NotFoundException,
+  Post,
   Req,
   UseGuards,
 } from '@nestjs/common';
 import { WishlistsService } from './wishlists.service';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { Request } from 'express';
+import { CreateWishlistItemDto } from './dto/create-wishlist-item.dto';
 
 @Controller('wishlists')
 export class WishlistsController {
@@ -23,5 +26,28 @@ export class WishlistsController {
     const wishlist =
       await this.wishlistsService.getOrCreateWishlistByUserIdService(user);
     return wishlist;
+  }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  async addProductToWishlist(
+    @Req() req: Request,
+    @Body() createWishlistDto: CreateWishlistItemDto,
+  ) {
+    const user = req.user;
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const item = await this.wishlistsService.addProductToWishlistService(
+      user,
+      createWishlistDto,
+    );
+
+    if (!item) {
+      throw new NotFoundException('Wishlist not found');
+    }
+
+    return item;
   }
 }
--- src/wishlists/wishlists.service.ts
+++ src/wishlists/wishlists.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Wishlist } from './entities/wishlist.entity';
 import { Repository } from 'typeorm';
@@ -6,12 +10,20 @@ import { UserResponseDto } from 'src/users/dto/user-response.dto';
 import { ResponseFormItf } from 'src/interfaces/response-form.interface';
 import { WishlistResponseDto } from './dto/wishlist-response.dto';
 import { plainToInstance } from 'class-transformer';
+import { CreateWishlistItemDto } from './dto/create-wishlist-item.dto';
+import { WishlistItemDto } from './dto/wishlist-item.dto';
+import { WishlistItem } from './entities/wishlist-item.entity';
+import { Product } from 'src/products/entities/product.entity';
 
 @Injectable()
 export class WishlistsService {
   constructor(
     @InjectRepository(Wishlist)
     private wishlistRepository: Repository<Wishlist>,
+    @InjectRepository(WishlistItem)
+    private wishlistItemRepository: Repository<WishlistItem>,
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
   ) {}
 
   async getOrCreateWishlistByUserIdService(
@@ -46,6 +58,45 @@ export class WishlistsService {
       data: plainToInstance(WishlistResponseDto, wishlist, {
         excludeExtraneousValues: true,
       }) as WishlistResponseDto,
+    };
+  }
+
+  async addProductToWishlistService(
+    user: UserResponseDto,
+    createWishlistDto: CreateWishlistItemDto,
+  ): Promise<ResponseFormItf<WishlistItemDto>> {
+    const userId = user.id;
+    if (!userId) throw new NotFoundException('User ID not found');
+
+    const wishlist = await this.wishlistRepository.findOne({
+      where: { user: { id: userId } },
+    });
+    if (!wishlist) throw new NotFoundException('Wishlist not found');
+
+    const product = await this.productRepository.findOne({
+      where: { id: createWishlistDto.productId },
+    });
+    if (!product) throw new NotFoundException('Product not found');
+
+    const exists = await this.wishlistItemRepository.findOne({
+      where: {
+        wishlist: { id: wishlist.id },
+        product: { id: product.id },
+      },
+    });
+    if (exists) throw new ConflictException('Product already in wishlist');
+
+    const wishlistItem = this.wishlistItemRepository.create({
+      product,
+      wishlist,
+    });
+
+    const createdItem = await this.wishlistItemRepository.save(wishlistItem);
+    return {
+      data: plainToInstance(WishlistItemDto, createdItem, {
+        excludeExtraneousValues: true,
+      }) as WishlistItemDto,
+      message: 'Product added to wishlist',
     };
   }
 }
